132bdd9085f039af9de7cd443095d86b4fb21f00
Robert Fairhead <bob@adhocteam.us>
2017-02-01 15:52:41 -0500

    fixing quotes

--- bin/git-clean-br.sh
+++ bin/git-clean-br.sh
@@ -5,13 +5,13 @@ git checkout master
 git fetch --prune
 
 IFS=$'\n'
-REMOTES=($(git for-each-ref --format='%(refname:short)' refs/remotes))
+REMOTES=( $(git for-each-ref --format="%(refname:short)" refs/remotes) )
 
 IFS=$' '
-git for-each-ref --format='%(refname:short) %(upstream:short)' refs/heads/ \
+git for-each-ref --format="%(refname:short) %(upstream:short)" refs/heads/ \
   | while read branch remote; do
-      if [[ ! ' ${REMOTES[@]} ' =~ ' ${remote} ' ]]; then
-        if [[ '${branch}' != 'master' ]]; then
+      if [[ ! " ${REMOTES[@]} " =~ " ${remote} " ]]; then
+        if [[ "${branch}" != "master" ]]; then
           echo $branch
         fi
       fi
