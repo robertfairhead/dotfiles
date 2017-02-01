53700c90b697bf3077472cf81ba8905fa0ae6463
Robert Fairhead <bob@adhocteam.us>
2017-02-01 15:46:36 -0500

    Adding extra git branch aliases

---
+++ bin/git-clean-br.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+set -euo pipefail
+
+git checkout master
+git fetch --prune
+
+IFS=$'\n'
+REMOTES=($(git for-each-ref --format='%(refname:short)' refs/remotes))
+
+IFS=$' '
+git for-each-ref --format='%(refname:short) %(upstream:short)' refs/heads/ \
+  | while read branch remote; do
+      if [[ ! ' ${REMOTES[@]} ' =~ ' ${remote} ' ]]; then
+        if [[ '${branch}' != 'master' ]]; then
+          echo $branch
+        fi
+      fi
+    done
