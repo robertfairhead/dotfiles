f1693ca6be36f76a9606e761b69744fc1074cea2
Robert Fairhead <bob@adhocteam.us>
2017-02-27 20:10:22 -0500

    Initial windows script

--- scripts/windows.sh
+++ scripts/windows.sh
@@ -4,3 +4,9 @@ cd $HOME
 
 ln -sf /mnt/c/Users/Bob/dotfiles dotfiles
 ln -sf /mnt/c/Users/Bob/Dropbox Dropbox
+
+if [[ ! -d /mnt/c/Users/Bob/src ]]; then
+    mkdir /mnt/c/Users/Bob/src
+fi
+
+ln -sf /mnt/c/Users/Bob/src src
