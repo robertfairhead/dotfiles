eea72bcdcb9d3b51466c0a63c4fe5c31e87587b7
Robert Fairhead <bob@adhocteam.us>
2017-10-19 15:28:28 -0500

    Some pre-os refresh clean-up

--- scripts/github.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-GH_TOKEN=$( git config --get github.token )
-
-http https://api.github.com/ \
-    Authorization:"token $GH_TOKEN"
--- scripts/yubikey.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#https://wikitech.wikimedia.org/wiki/Yubikey-SSH
-
-if [ $OS == "linux" ]; then
-    sudo apt-get install yubikey-personalization yubico-piv-tool opensc pcscd
-fi
-
