c32420d47b3478a68b779692eb69f4fc3e183d09
Robert Fairhead <robert.fairhead@aptible.com>
2020-06-16 14:39:58 -0500

    Recent updates

--- app_install/arch_repo.sh
+++ app_install/arch_repo.sh
@@ -3,62 +3,61 @@
 set -euo pipefail
 
 check_yay() {
-    if ! command -v yay >/dev/null; then
-        echo "Installing yay"
-
-        local tmp=$(mktemp -d)
-        trap "rm -rf $tmp" EXIT
-
-        pushd ${tmp} >/dev/null
-
-        git clone https://aur.archlinux.org/yay.git
-        cd yay
-        makepkg -si
-
-        popd >/dev/null
-    fi
-}
-
-create_pkg() {
-    echo "Building package locally"
+  if ! command -v yay >/dev/null; then
+    echo "Installing yay"
 
     local tmp=$(mktemp -d)
-    trap "rm -rf $tmp" EXIT
+    trap 'rm -rf $tmp' EXIT
 
     pushd ${tmp} >/dev/null
 
-    cp $HOME/dotfiles/app_install/PKGBUILD .
-    makepkg -s
-
-    cp *.pkg.tar.xz $HOME/dotfiles/app_install
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
 
     popd >/dev/null
+  fi
+}
+
+create_pkg() {
+  echo "Building package locally"
+
+  local tmp=$(mktemp -d)
+  trap 'rm -rf $tmp' EXIT
+
+  pushd ${tmp} >/dev/null
+
+  cp $HOME/dotfiles/app_install/PKGBUILD .
+  makepkg -s
+
+  cp *.pkg.tar.xz $HOME/dotfiles/app_install
+
+  popd >/dev/null
 }
 
 make_local_repo() {
 
-    check_yay
+  check_yay
 
-    create_pkg
+  create_pkg
 
-    local repo_dir="$HOME/.local/lib/bob-base"
-    local repo_file="${repo_dir}/bob-base.db.tar.gz"
-    mkdir -p "${repo_dir}"
+  local repo_dir="$HOME/.local/lib/bob-base"
+  local repo_file="${repo_dir}/bob-base.db.tar.gz"
+  mkdir -p "${repo_dir}"
 
-    if [[ -f "${repo_file}" ]]; then
-        rm -f "${repo_file}"
-    fi
+  if [[ -f "${repo_file}" ]]; then
+    rm -f "${repo_file}"
+  fi
 
-    repo-add "${repo_file}" *.pkg.tar.xz
-    mv *.pkg.tar.xz "${repo_dir}"
+  repo-add "${repo_file}" *.pkg.tar.xz
+  mv *.pkg.tar.xz "${repo_dir}"
 
-    if grep -q "bob-base" /etc/pacman.conf; then
-        return 0
-    else
-        sudo cp /etc/pacman.conf /etc/pacman.conf.old
-        echo -e "[bob-base]\nSigLevel = Optional TrustAll\nServer = file:///${repo_dir}/" | sudo tee -a /etc/pacman.conf
-    fi
+  if grep -q "bob-base" /etc/pacman.conf; then
+    return 0
+  else
+    sudo cp /etc/pacman.conf /etc/pacman.conf.old
+    echo -e "[bob-base]\nSigLevel = Optional TrustAll\nServer = file:///${repo_dir}/" | sudo tee -a /etc/pacman.conf
+  fi
 }
 
 make_local_repo
-
