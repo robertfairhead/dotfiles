b69e5885f9b31b8fb1460012402800eeeb6d3ab5
Robert Fairhead <bob@fairhead.dev>
2025-01-10 13:46:33 -0600

    Mac setup reworked

--- macos/linuxish.sh
+++ macos/linuxish.sh
@@ -3,43 +3,43 @@
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do
+	sudo -n true
+	sleep 60
+	kill -0 "$$" || exit
+done 2>/dev/null &
 
 # Check for Homebrew,
 # Install if we don't have it
 if ! command -v brew &>/dev/null; then
-    echo "Installing homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	echo "Installing homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew update
 brew upgrade
-
-if ! command -v mas &>/dev/null; then
-    echo "Install mas-cli for reinstalling App Store apps"
-    brew install mas
-fi
-
 brew bundle
 brew cleanup
 
+sudo mkdir -p /usr/local/bin
+sudo ln -sf "/opt/homebrew/bin/bash" "/usr/local/bin/bash"
+
+bins=(dircolors sed make tr fold)
+for bin in "${bins[@]}"; do
+	sudo ln -sf "/opt/homebrew/bin/g${bin}" "/usr/local/bin/${bin}"
+done
+
 # Installed the new shell, now we have to activate it
 if [[ "$SHELL" != "/usr/local/bin/bash" ]]; then
-    echo "Setting new bash as our shell"
-    sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
-    # Change to the new shell, prompts for password
-    chsh -s /usr/local/bin/bash
+	echo "Setting new bash as our shell"
+	sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+	# Change to the new shell, prompts for password
+	chsh -s /usr/local/bin/bash
 fi
 
 # Install alacritty terminfo entries
 if ! infocmp alacritty &>/dev/null; then
-    curl -sSfo /tmp/alacritty.info https://raw.githubusercontent.com/jwilm/alacritty/master/extra/alacritty.info
-    sudo tic -xe alacritty,alacritty-direct /tmp/alacritty.info
-    rm -f /tmp/alacritty.info
+	curl -sSfo /tmp/alacritty.info https://raw.githubusercontent.com/jwilm/alacritty/master/extra/alacritty.info
+	sudo tic -xe alacritty,alacritty-direct /tmp/alacritty.info
+	rm -f /tmp/alacritty.info
 fi
-
-bins=(dircolors sed make tr fold)
-
-for bin in ${bins[@]}; do
-    sudo ln -sf /usr/local/bin/g${bin} /usr/local/bin/${bin}
-done
--- macos/setup.sh
+++ macos/setup.sh
@@ -1,24 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ## Much borrowed from https://mths.be/osx
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
 
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do
+	sudo -n true
+	sleep 60
+	kill -0 "$$" || exit
+done 2>/dev/null &
 
 ###############################################################################
 # General UI/UX                                                               #
 ###############################################################################
-
-read -p "Enter hostname: " NEWNAME
-
-# Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName $NEWNAME
-sudo scutil --set HostName $NEWNAME
-sudo scutil --set LocalHostName $NEWNAME
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $NEWNAME
 
 # Only show scrollbars when scrolling
 defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
@@ -40,6 +40,10 @@ defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 # Automatically quit printer app once the print jobs complete
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
+# Display ASCII control characters using caret notation in standard text views
+# Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
+defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
+
 # Set Help Viewer windows to non-floating mode
 defaults write com.apple.helpviewer DevMode -bool true
 
@@ -49,6 +53,21 @@ sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo Hos
 
 # Restart automatically if the computer freezes
 sudo systemsetup -setrestartfreeze on
+
+# Disable automatic capitalization as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
 # Check for software updates daily, not just once per week
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
@@ -80,6 +99,8 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightC
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
 
+# Increase sound quality for Bluetooth headphones/headsets
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
 
 ###############################################################################
 # Screen                                                                      #
@@ -94,7 +115,7 @@ defaults write com.apple.screencapture type -string "png"
 defaults write com.apple.screencapture disable-shadow -bool true
 
 # Enable subpixel font rendering on non-Apple LCDs
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
+defaults write NSGlobalDomain AppleFontSmoothing -int 1
 
 ###############################################################################
 # Finder                                                                      #
@@ -129,6 +150,9 @@ defaults write com.apple.finder ShowPathbar -bool true
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
 # When performing a search, search the current folder by default
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
@@ -160,7 +184,7 @@ defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
 # Show the ~/Library folder
-chflags nohidden ~/Library
+chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
 
 # Expand the following File Info panes:
 # “General”, “Open with”, and “Sharing & Permissions”
@@ -253,16 +277,6 @@ defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
 defaults write com.apple.DiskUtility advanced-image-options -bool true
 
 ###############################################################################
-# Mac App Store                                                               #
-###############################################################################
-
-# Enable the WebKit Developer Tools in the Mac App Store
-defaults write com.apple.appstore WebKitDeveloperExtras -bool true
-
-# Enable Debug Menu in the Mac App Store
-defaults write com.apple.appstore ShowDebugMenu -bool true
-
-###############################################################################
 # Photos                                                                      #
 ###############################################################################
 
@@ -279,6 +293,5 @@ defaults write com.google.Chrome.canary DisablePrintPreview -bool true
 # Expand the print dialog by default
 defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
 defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
-
 
 echo "Done. Note that some of these changes require a logout/restart to take effect."
